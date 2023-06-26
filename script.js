db7f3f8f1e95070e3d542e3d854fe52ecdf8e509
Aditya Harde <110459473+Aditya049823@users.noreply.github.com>
2023-06-26 11:40:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function helper(){
+	return new Promise((resolve) =>{
+		setTimeout(() =>{
+			resolve("Hello, world!");
+	},1000)
+	});
+}
+
+let divElement=document.getElementById("output");
+helper().then((result) =>{
+	divElement.innerHTML=result;
+});
